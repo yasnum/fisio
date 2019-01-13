2757635f3152c3d32551c4392827cb6ea18eab9e
yasnum <putriyasnum@gmail.com>
2019-01-13 22:51:04 +0700

    resetpswd

--- fisio/settings.py
+++ fisio/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 #LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
