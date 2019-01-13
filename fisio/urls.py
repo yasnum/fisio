150038e7408d933207c24fe84aacf283634690b6
yasnum <putriyasnum@gmail.com>
2019-01-13 10:04:02 +0700

    accounts

--- fisio/urls.py
+++ fisio/urls.py
@@ -19,6 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  
+] 
