3ada130569ce064e53d5d6f1d83147a9473f04d5
yasnum <putriyasnum@gmail.com>
2019-01-13 20:56:47 +0700

    login

--- fisio/settings.py
+++ fisio/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'fisio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # news
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# my_project/settings.py
+LOGIN_REDIRECT_URL = '/'
